921ecff25f344872934ac182dfd1756facd67b03
Katarina Sheremet <kate.romanowskaya@gmail.com>
2020-04-19 09:05:38 +0200

    Generate golden on Mac

--- test/golden_widget_test.dart
+++ test/golden_widget_test.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:goldens_codemagic/main.dart';
 
 void main() {
   testWidgets('Golden test', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
-    await expectLater(find.byType(MyApp), matchesGoldenFile('main.png'));
-  });
+    await expectLater(
+      find.byType(MyApp),
+      matchesGoldenFile('goldens/main.png'),
+    );
+  }, skip: !Platform.isMacOS);
 }
